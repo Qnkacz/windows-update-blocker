3bf6ef320f874cdb9388b48d0445f4adf4735db8
Qnkacz <wasik.bartosz@outlook.com>
2020-05-12 14:29:03 +0200

    added timer options and hiding to system tray

--- windows update blocker/MainWindow.xaml.cs
+++ windows update blocker/MainWindow.xaml.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Management;
 using System.ServiceProcess;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -25,6 +29,10 @@ namespace windows_update_blocker
     public partial class MainWindow : Window
     {
         ServiceController[] scServices;
+        ServiceController wu = null;
+        private static readonly Regex _regex = new Regex("[^0-9]");
+        public bool shouldloop=false;
+        System.Timers.Timer timer = null;
         private void SetStartup(bool mode)
         {
             RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", mode);
@@ -42,6 +50,13 @@ namespace windows_update_blocker
             try
             {
                 scServices = ServiceController.GetServices();
+                foreach (var item in scServices)
+                {
+                    if (item.DisplayName == "Windows Update")
+                    {
+                        wu = item;
+                    }
+                }
             }
             catch
             {
@@ -63,45 +78,83 @@ namespace windows_update_blocker
 
         private void Block_Click(object sender, RoutedEventArgs e)
         {
-            foreach (var item in scServices)
-            {
-                if(item.DisplayName == "Windows Update")
-                {
-                    item.Refresh();
-                    if (item.Status.Equals(ServiceControllerStatus.Stopped) || item.Status.Equals(ServiceControllerStatus.StopPending))
-                    {
-                        MessageBox.Show("Allready stopped babyyyy","Status",MessageBoxButton.OK);
-                    }
-                    else
-                    {
-                        item.Stop();
-                    }
-                }
-            }
+            shouldloop = true;
+            ChangeStatusOfService(false);
+            loopBlock();
         }
 
         private void unBlock_Click(object sender, RoutedEventArgs e)
         {
-            foreach (var item in scServices)
-            {
-                if (item.DisplayName == "Windows Update")
-                {
-                    item.Refresh();
-                    if (item.Status.Equals(ServiceControllerStatus.Running))
-                    {
-                        MessageBox.Show("Allready running babyyyy", "Status", MessageBoxButton.OK);
-                    }
-                    else
-                    {
-                        item.Start();
-                        MessageBox.Show("started");
-                    }
-                }
-            }
+            shouldloop = false;
+            ChangeStatusOfService(true);
         }
         private void ChangeStatusOfService(bool status)
         {
+            wu.Refresh();
+            if (status == false)
+            {
+                if (wu.Status.Equals(ServiceControllerStatus.Stopped) || wu.Status.Equals(ServiceControllerStatus.StopPending))
+                {
+                    
+                }
+                else
+                {
+                    wu.Stop();
+                }
+            }
+            else
+            {
+                if (wu.Status.Equals(ServiceControllerStatus.Running))
+                {
+                    
+                }
+                else
+                {
+                    wu.Start();
+                }
+            }
+           
+        }
 
+        private void time_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = _regex.IsMatch(e.Text);
+        }
+
+        private void time_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key==Key.Space)
+            {
+                time.Text = time.Text.Substring(0, time.Text.Length - 1);
+            }
+        }
+        private void loopBlock()
+        {
+            if(time.Text!=null)
+            {
+                while (shouldloop == true)
+                {
+                    Thread.Sleep(Int32.Parse(time.Text) * 60 * 1000);
+                    ChangeStatusOfService(false);
+                }
+            }
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized) this.Hide();
+
+            base.OnStateChanged(e);
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            // setting cancel to true will cancel the close request
+            // so the application is not closed
+            e.Cancel = true;
+
+            this.Hide();
+
+            base.OnClosing(e);
         }
     }
 }

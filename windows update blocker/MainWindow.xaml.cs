a600bbd58f27c6f19cf002b2f0c4ec9e3982eea0
Qnkacz <wasik.bartosz@outlook.com>
2020-05-13 18:03:11 +0200

    Added run as admin support

--- windows update blocker/MainWindow.xaml.cs
+++ windows update blocker/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.ServiceProcess;
@@ -8,7 +7,8 @@ using System.Windows;
 using System.Windows.Input;
 using Microsoft.Win32;
 using System.Windows.Threading;
-using System.Windows.Controls;
+using System.Security.Principal;
+using System.Reflection;
 
 namespace windows_update_blocker
 {
@@ -40,8 +40,43 @@ namespace windows_update_blocker
         }
         public MainWindow()
         {
-            InitializeComponent();
-            GetServices();
+            var wi = WindowsIdentity.GetCurrent();
+            var wp = new WindowsPrincipal(wi);
+
+            bool runAsAdmin = wp.IsInRole(WindowsBuiltInRole.Administrator);
+
+            if (!runAsAdmin)
+            {
+                // It is not possible to launch a ClickOnce app as administrator directly,
+                // so instead we launch the app as administrator in a new process.
+                var processInfo = new ProcessStartInfo(Assembly.GetExecutingAssembly().CodeBase);
+
+                // The following properties run the new process as administrator
+                processInfo.UseShellExecute = true;
+                processInfo.Verb = "runas";
+
+                // Start the new process
+                try
+                {
+                    Process.Start(processInfo);
+                }
+                catch (Exception)
+                {
+                    // The user did not allow the application to run as administrator
+                    MessageBox.Show("Sorry, but I don't seem to be able to start " +
+                       "this program with administrator rights!");
+                }
+
+                // Shut down the current process
+                System.Windows.Application.Current.Shutdown();
+            }
+            else
+            {
+                // We are running as administrator
+                InitializeComponent();
+                GetServices();
+            }
+           
         }
 
         private void GetServices()
